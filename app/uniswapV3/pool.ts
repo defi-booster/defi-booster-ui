906429a470a79bd73cc435f2ca2ce5c5ef6d5245
rskrzydelski <r.skrzydelski@gmail.com>
2024-08-22 11:06:53 +0200

    feature #24: handle user position fee

--- app/uniswapV3/pool.ts
+++ app/uniswapV3/pool.ts
@@ -12,7 +12,9 @@ export async function getPoolInfo(
     chainId: number,
     tokenIn: Token,
     tokenOut: Token,
-    poolFee: number
+    poolFee: number,
+    tickUpper: number,
+    tickLower: number
 ): Promise<GeneralPoolInfo> {
     let factoryAddress: string
     try {
@@ -38,15 +40,29 @@ export async function getPoolInfo(
         provider
     )
 
-    const [token0, token1, fee, tickSpacing, liquidity, slot0] =
-        await Promise.all([
-            poolContract.token0(),
-            poolContract.token1(),
-            poolContract.fee(),
-            poolContract.tickSpacing(),
-            poolContract.liquidity(),
-            poolContract.slot0(),
-        ])
+    const [
+        token0,
+        token1,
+        fee,
+        tickSpacing,
+        liquidity,
+        slot0,
+        feeGrowthGlobal0X128,
+        feeGrowthGlobal1X128,
+        ticksUpperOutput,
+        ticksLowerOutput,
+    ] = await Promise.all([
+        poolContract.token0(),
+        poolContract.token1(),
+        poolContract.fee(),
+        poolContract.tickSpacing(),
+        poolContract.liquidity(),
+        poolContract.slot0(),
+        poolContract.feeGrowthGlobal0X128(),
+        poolContract.feeGrowthGlobal1X128(),
+        poolContract.ticks(tickUpper),
+        poolContract.ticks(tickLower),
+    ])
 
     return {
         token0Address: token0,
@@ -56,5 +72,11 @@ export async function getPoolInfo(
         liquidity: liquidity,
         sqrtPriceX96: slot0[0],
         tick: slot0[1],
+        feeGrowthGlobalX128_0: feeGrowthGlobal0X128,
+        feeGrowthGlobalX128_1: feeGrowthGlobal1X128,
+        tickUpperFeeGrowthOutsideX128_0: ticksUpperOutput[2],
+        tickUpperFeeGrowthOutsideX128_1: ticksUpperOutput[3],
+        tickLowerFeeGrowthOutsideX128_0: ticksLowerOutput[2],
+        tickLowerFeeGrowthOutsideX128_1: ticksLowerOutput[3],
     }
 }
--- app/uniswapV3/types.ts
+++ app/uniswapV3/types.ts
@@ -1,3 +1,4 @@
+// data from UniswapV3Pool contract
 export interface GeneralPoolInfo {
     token0Address: string
     token1Address: string
@@ -6,11 +7,16 @@ export interface GeneralPoolInfo {
     liquidity: bigint
     sqrtPriceX96: bigint
     tick: bigint
+    feeGrowthGlobalX128_0: bigint
+    feeGrowthGlobalX128_1: bigint
+    tickUpperFeeGrowthOutsideX128_0: bigint
+    tickUpperFeeGrowthOutsideX128_1: bigint
+    tickLowerFeeGrowthOutsideX128_0: bigint
+    tickLowerFeeGrowthOutsideX128_1: bigint
 }
 
+// data from NonfungiblePositionManager contract
 export interface UserPositionInfo {
-    // nonce: bigint // the nonce for permits
-    // operator: string // the address that is approved for spending
     token0Address: string
     token1Address: string
     token0Symbol: string
@@ -21,10 +27,10 @@ export interface UserPositionInfo {
     tickLower: bigint
     tickUpper: bigint
     liquidity: bigint
-    feeGrowthInside0LastX128: bigint // fee growth of token0 inside the tick range as of the last mint/burn/poke,
-    feeGrowthInside1LastX128: bigint // fee growth of token1 inside the tick range as of the last mint/burn/poke,
-    tokensOwed0: bigint // the computed amount of token0 owed to the position as of the last mint/burn/poke
-    tokensOwed1: bigint // the computed amount of token1 owed to the position as of the last mint/burn/poke
+    feeGrowthInsideLastX128_0: bigint
+    feeGrowthInsideLastX128_1: bigint
+    tokensOwed0: bigint
+    tokensOwed1: bigint
 }
 
 export interface PositionData {
